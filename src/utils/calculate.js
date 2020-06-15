c417cfc8f2732db1796969bc99289b41e12d378e
ccostoso <carlos.costoso@outlook.com>
2020-06-14 20:48:45 -0400

    Bug fixes for src/utils/calculate.js

--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -59,16 +59,18 @@ module.exports = {
     analyze: function (pricesArr) {
         let lastPriceIdx = pricesArr.indexOf(pricesArr.find(ele => ele === 0)) >= 0 ? pricesArr.indexOf(pricesArr.find(ele => ele === 0)) - 1 : pricesArr.length - 1;
         const increases = Object.values(countSubsequentIncreases(pricesArr));
+        console.log("increases", increases);
 
         if (lastPriceIdx < 2) return ["Unsure", "unsure", "certain"];
         if (lastPriceIdx > 7 && (!increases.length)) return ["Declining", "declining", "certain"];
 
         if (increases.length > 1) return ["Random", "random", "certain"]; 
 
-        if (increases.length && increases[0]["size"] === 3 && increases[0]["end"]) return ["Random", "random", "certain"];
+        if (increases.length && increases[0]["size"] === 3 && (pricesArr[lastPriceIdx] > increases[0]["end"].value) && pricesArr[lastPriceIdx + 1] > 0) return ["Random", "random", "certain"];
         if (increases.length && increases[0]["size"] === 2) return ["Unsure", "unsure", "notDeclining"];
         if (increases.length && increases[0]["size"] > 2) return spikeBigOrSmall(increases[0]["steps"]);
+        if (increases.length) return ["Unsure", "unsure", "notDeclining"];
 
-        return ["Unsure", "unsure", "notDeclining"];
+        return ["Unsure", "unsure", "certain"];
     }
 }
